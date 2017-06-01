dcfe90d3d3808f6bc80f286fe14de3e42cc2fec9
iLGunners <mujtabanowshad@gmail.com>
2017-06-01 22:49:35 +0600

    classic version implementation

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func ackermann(m, n uint64) uint64 {
+func ackermann(m, n int) int {
 	if m == 0 {
 		return n + 1
 	} else if n == 0 {
@@ -19,7 +19,7 @@ func main() {
 
 	for m := 0; m < 5; m++ {
 		for n := 0; n < 16-m; n++ {
-
+			fmt.Println("ackermann(", m, ",", n, ") = ", ackermann(m, n))
 		}
 	}
 }
